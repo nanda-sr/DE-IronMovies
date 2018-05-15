0dd764f743810a4a2e76c97725e1f4a1cbf364e0
Fernanda <mfernandasrmz8201@gmail.com>
2018-05-14 20:40:41 -0500

    done

--- src/App.js
+++ src/App.js
@@ -1,21 +1,46 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+// import Action from './components/Action';
+
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+  state = {
+    title: this.props.title,
+    image: this.props.image,
+    description: this.props.description,
+    genre: this.props.genre,
+    movie: [],
+    movieDrama: [],
+    movieComedy: []
 
+  }
+
+  componentWillMount(){
+    fetch('https://api.themoviedb.org/3/genre/28/movies?api_key=488fd9ef8067ac8018d3e589faaeea82&language=en-US&include_adult=false&sort_by=created_at.asc')
+    .then(r=>r.json())
+    .then(movie=>{
+      this.setState({movie:movie.results})
+    });
+  }
+render(){
+  const link = 'https://image.tmdb.org/t/p/w500/';
+  return(
+    <div className="moviesInfo">
+      <a href="#/action">Action</a>
+      <span href="#">Comedy</span>
+      <span href="#">Drama</span>
+      <div className="movies">
+      {this.state.movie.map(m=>{
+        return (   
+        <div className="movie-info"> 
+          <img className="poster" src= {link + m.poster_path} alt="Poster" />
+          <h2 className="movie-title">{m.original_title}</h2>
+        </div>
+        )
+      })}
+      </div>
+    </div>
+  )
+}
+}
 export default App;
---
+++ src/components/Action.js
@@ -0,0 +1,25 @@
+// import React, {Component} from 'react';
+
+// class Action extends Component {
+//   render(){
+//   const link = 'https://image.tmdb.org/t/p/w500/';
+//   return(
+//     <div className="moviesInfo">
+//       <a href="#/action">Action</a>
+//       <span href="#">Comedy</span>
+//       <span href="#">Drama</span>
+//       <div className="movies">
+//       {this.state.movie.map(m=>{
+//         return (   
+//         <div className="movie-info"> 
+//              <img className="poster" src= {link + m.poster_path} alt="Poster" />
+//              <h2 className="movie-title">{m.original_title}</h2>
+//         </div>
+//         )
+//       })}
+//       </div>
+//     </div>
+//     )
+//   }
+// }
+//   export default Action;
